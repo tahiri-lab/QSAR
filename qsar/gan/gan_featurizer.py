932835d8c8e6eeaf967db4e054ea160e770886b5
moben1 <mohammed.benabbassi@usherbrooke.ca>
2023-11-08 09:54:26 +0900

    implement the gan

--- qsar/gan/gan_featurizer.py
+++ qsar/gan/gan_featurizer.py
@@ -9,7 +9,7 @@ class QsarGanFeaturizer(MolGanFeaturizer):
         super(QsarGanFeaturizer, self).__init__(**kwargs)
 
     @staticmethod
-    def get_atom_count(smiles_val: str) -> int:
+    def _get_atom_count(smiles_val: str) -> int:
         """
         Get the number of atoms in a SMILES string.
         Args:
@@ -31,25 +31,26 @@ class QsarGanFeaturizer(MolGanFeaturizer):
         Returns:
             Maximum number of atoms in the SMILES strings.
         """
-        smiles['atom_count'] = smiles['smiles'].apply(self.get_atom_count)
-        self.vertices = int(smiles['atom_count'].quantile(quantile))
-        return self.vertices
+        smiles['atom_count'] = smiles['smiles'].apply(self._get_atom_count)
+        self.max_atom_count = int(smiles['atom_count'].quantile(quantile))
+        return self.max_atom_count
 
-    def filter_smiles(self, smiles: pd.DataFrame, num_atoms: int = None) -> pd.DataFrame:
+    def _filter_smiles(self, smiles: pd.DataFrame, num_atoms: int = None) -> list:
         """
         Filter SMILES strings based on the number of atoms.
         Args:
             smiles: DataFrame containing SMILES strings.
             num_atoms: Number of atoms to be used for filtering.
         Returns:
-            DataFrame containing SMILES strings with less than num_atoms.
+            list containing SMILES strings with less than num_atoms.
         """
         if num_atoms is None:
-            num_atoms = self.vertices
+            num_atoms = self.max_atom_count
 
-        return smiles[smiles['smiles'].apply(self.get_atom_count) < num_atoms]
+        return smiles[smiles['smiles'].apply(self._get_atom_count) < num_atoms].tolist()
 
-    def get_unique_smiles(self, smiles: pd.DataFrame) -> list:
+    @staticmethod
+    def get_unique_smiles(smiles:  np.ndarray) -> list:
         """
         Get unique SMILES strings.
         Args:
@@ -58,19 +59,20 @@ class QsarGanFeaturizer(MolGanFeaturizer):
         Returns:
             List of unique SMILES strings.
         """
-        unique_smiles = smiles['smiles'].dropna().unique()
+        unique_smiles = np.unique(smiles)
         return [sm for sm in unique_smiles if Chem.MolFromSmiles(sm) is not None]
 
-    def get_features(self, datapoints, log_every_n=1000, **kwargs) -> np.ndarray:
+    def get_features(self, smiles, log_every_n=1000, **kwargs) -> np.ndarray:
         """
         Get features for a list of SMILES strings.
         Args:
-            datapoints: List of SMILES strings.
+            smiles: List of SMILES strings.
             log_every_n: Logging messages reported every `log_every_n` samples.
             **kwargs: Additional keyword arguments.
         Returns:
 
         """
-        features = self.featurize(datapoints, log_every_n=log_every_n, **kwargs)
+        filtered_smiles = self._filter_smiles(smiles)
+        features = self.featurize(filtered_smiles, log_every_n=log_every_n, **kwargs)
         valid_features = [f for f in features if isinstance(f, GraphMatrix)]
         return np.array(valid_features)
---
+++ qsar/gan/qsar_gan.py
@@ -0,0 +1,87 @@
+from deepchem.models.molgan import BasicMolGANModel
+from deepchem.models.optimizers import ExponentialDecay
+from tensorflow import one_hot
+import deepchem as dc
+
+from qsar.gan.gan_featurizer import QsarGanFeaturizer
+
+
+class QsarGan(BasicMolGANModel):
+    def __init__(self,
+                 learning_rate: ExponentialDecay,
+                 edges: int = 5,
+                 vertices: int = 9,
+                 nodes: int = 5,
+                 embedding_dim: int = 10,
+                 dropout_rate: float = 0.0,
+                 **kwargs):
+        """
+        Initialize the model
+
+        Parameters
+        ----------
+        learning_rate: ExponentialDecay
+            Learning rate scheduler
+        edges: int, default 5
+            Number of bond types includes BondType.Zero
+        vertices: int, default 9
+            Max number of atoms in adjacency and node features matrices
+        nodes: int, default 5
+            Number of atom types in node features matrix
+        embedding_dim: int, default 10
+            Size of noise input array
+        dropout_rate: float, default = 0.
+            Rate of dropout used across whole model
+        name: str, default ''
+            Name of the model
+        """
+
+        self.learning_rate = learning_rate
+        self.edges = edges
+        self.vertices = vertices
+        self.nodes = nodes
+        self.embedding_dim = embedding_dim
+        self.dropout_rate = dropout_rate
+
+        self.gan_featurizer = QsarGanFeaturizer(max_atom_count=self.vertices)
+        super(BasicMolGANModel, self).__init__(**kwargs)
+
+    def _iterbatches(self, epochs, features):
+        dataset = dc.data.NumpyDataset([x.adjacency_matrix for x in features], [x.node_features for x in features])
+        for i in range(epochs):
+            for batch in dataset.iterbatches(batch_size=self.batch_size, pad_batches=True):
+                adjacency_tensor = one_hot(batch[0], self.edges)
+                node_tensor = one_hot(batch[1], self.nodes)
+                yield {self.data_inputs[0]: adjacency_tensor, self.data_inputs[1]: node_tensor}
+
+    def fit_predict(self, smiles, epochs=32, generator_steps=0.2, checkpoint_interval=5000, number_to_generate=1000):
+        """
+        Fit the model and return the generated molecules.
+
+        Parameters
+        ----------
+        smiles: np.ndarray
+            Array of SMILES strings.
+        epochs: int, default 32
+            Number of epochs to train the model
+        generator_steps: float, default 0.2
+            Number of generator steps per discriminator step
+        checkpoint_interval: int, default 5000
+            Number of steps between saving model checkpoints
+        number_to_generate: int, default 1000
+            Number of molecules to generate
+        Returns
+        -------
+        Generated molecules
+        """
+
+        self.vertices = self.gan_featurizer.determine_atom_count(smiles)
+        features = self.gan_featurizer.get_features(smiles)
+
+        self.fit_gan(self._iterbatches(epochs, features), generator_steps=generator_steps,
+                     checkpoint_interval=checkpoint_interval)
+
+        generated_data = self.predict_gan_generator(number_to_generate)
+
+        generated_data = self.gan_featurizer.defeaturize(generated_data)
+        return self.gan_featurizer.get_unique_smiles(generated_data)
