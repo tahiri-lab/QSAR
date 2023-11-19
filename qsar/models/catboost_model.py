6b65b594b5782dbeff3a6b7a17a3cb84d73c0673
moben1 <aekmedbenabbassi@gmail.com>
2023-11-19 18:26:35 -0500

    add catboost model

---
+++ qsar/models/catboost_model.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from optuna import Trial
+from catboost import CatBoostRegressor
+
+from qsar.models.model import Model
+from qsar.utils.cross_validator import CrossValidator
+
+
+class CatboostModel(Model):
+    def __init__(self, max_iter: int = Model.DEFAULT_MAX_ITER, random_state: int = Model.DEFAULT_RANDOM_STATE,
+                 params=None):
+        """
+           Initialize the XGBoost model.
+
+           Parameters:
+           - max_iter: Maximum number of iterations for convergence.
+           - random_state: Seed for reproducibility.
+        """
+        super().__init__()
+        self.model = CatBoostRegressor(random_state=random_state)
+        self.params = params
+
+    def optimize_hyperparameters(self, trial: Trial, df: pd.DataFrame) -> float:
+        """
+        Optimize the hyperparameters of the XGBoost model using the given trial and data.
+
+        Parameters:
+        - trial: Optuna trial for hyperparameter optimization.
+        - df: Data for cross-validation.
+
+        Returns:
+        - Cross-validation score.
+        """
+        self.params = {
+            "objective": trial.suggest_categorical("objective", ["Logloss", "CrossEntropy"]),
+            'learning_rate': trial.suggest_loguniform('learning_rate', 0.001, 0.3),
+            "colsample_bylevel": trial.suggest_float("colsample_bylevel", 0.01, 0.1),
+            "max_depth": trial.suggest_int("max_depth", 1, 15),
+            "boosting_type": trial.suggest_categorical("boosting_type", ["Ordered", "Plain"]),
+            "bootstrap_type": trial.suggest_categorical("bootstrap_type", ["Bayesian", "Bernoulli", "MVS"]),
+        }
+
+        self.model.set_params(**self.params)
+
+        estimator = CrossValidator(df)
+        return estimator.cross_value_score(self.model)
