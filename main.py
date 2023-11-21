d41136ce025fa692c9ee3ef3e0ff03393d54e21f
moben1 <aekmedbenabbassi@gmail.com>
2023-11-21 14:35:18 -0500

    update main file

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import neptune.new as neptune
 from qsar.utils.extractor import Extractor
 from qsar.preprocessing.custom_preprocessing import PreprocessingPipeline
 
+MODELS_PATH = "/qsar/models/"
+
 
 def main():
+    # https://neptune.ai/blog/building-ml-model-training-pipeline
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default="compare_all_models.yaml.yaml", help="Path to the config file")
     args = parser.parse_args()
@@ -30,6 +33,11 @@ def main():
 
         # model pipeline
         # todo: add the model pipeline and the model training
+        for model in config["models"]:
+            model_name = model["name"]
+            model_params = model["params"]
+            model_path = os.path.join(MODELS_PATH, model_name).join("_model.py")
+            model_class = model_name.capitalize() + "Model"
 
         # models comparison
         # todo: compare the models and log the results to neptune
