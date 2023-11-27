56812c5530c50777902e53dc133f34988bbdbfc9
moben1 <aekmedbenabbassi@gmail.com>
2023-11-26 22:59:05 -0500

    document extractor.py

--- qsar/utils/extractor.py
+++ qsar/utils/extractor.py
@@ -5,26 +5,36 @@ import pandas as pd
 
 
 class Extractor:
+
+    """
+    Class for cross-validation related functionalities.
+
+    :param paths: Dictionary of {str: str} pairs where the key is the name of the dataframe and the value is the path to the CSV file.
+    :type paths: Dict[str, str]
+
+    :ivar dfs: Extracted DataFrames from the paths provided during initialization.
+    :vartype dfs: Dict[str, pd.DataFrame]
+    """
+
     def __init__(self, paths: Dict[str, str]):
         """
-        Parameters
-        ----------
-        paths : dict of {str: str} pairs where the key is the name of the dataframe and the value is the path to
-        the csv file.
+        Initialize the Extractor instance.
+
+        :param paths: Dictionary of {str: str} pairs where the key is the name of the dataframe and the value is the path to the CSV file.
+        :type paths: Dict[str, str]
         """
         self.paths = paths
         self.dfs = self.extract_dfs(self.paths)
 
     def get_df(self, name: str) -> pd.DataFrame:
         """
-        Get a df by its name
-        Parameters
-        ----------
-        name : str name of the df.
+        Get a DataFrame by its name.
 
-        Returns
-        -------
-        found dataframe
+        :param name: Name of the DataFrame.
+        :type name: str
+        :returns: The DataFrame associated with the given name.
+        :rtype: pd.DataFrame
+        :raises KeyError: If the name does not exist in the stored DataFrames.
         """
         if name not in self.dfs.keys():
             raise KeyError(f"{name} deos not exist.")
@@ -32,16 +42,13 @@ class Extractor:
 
     def extract_dfs(self, paths: Dict[str, str] = None) -> Dict[str, pd.DataFrame]:
         """
-        Extracts dataframes from a dictionary of {name: path} pairs.
-        Parameters
-        ----------
-        paths : dict of {str: str} pairs where the key is the name of the dataframe and the value is the path to
-        the csv file.
+        Extracts DataFrames from a dictionary of {name: path} pairs.
 
-        Returns
-        -------
-        dict of {str: pd.DataFrame} pairs where the key is the name of the dataframe and the value is the dataframe.
-
+        :param paths: Dictionary of {str: str} pairs where the key is the name of the dataframe and the value is the path to the CSV file. If not provided, defaults to the paths provided at initialization.
+        :type paths: Dict[str, str], optional
+        :returns: Dictionary of {str: pd.DataFrame} pairs where the key is the name of the dataframe and the value is the DataFrame itself.
+        :rtype: Dict[str, pd.DataFrame]
+        :raises FileNotFoundError: If a path in the dictionary does not exist.
         """
         self.dfs = dict()
         for name, path in paths.items():
@@ -54,17 +61,12 @@ class Extractor:
         self, y_col: str
     ) -> Tuple[Dict[str, pd.DataFrame], Dict[str, pd.DataFrame]]:
         """
-        Splits the dataframes into X and y dataframes.
-        Parameters
-        ----------
-        y_col : str
-            name of the column to be used as the y values.
+        Splits the DataFrames into X and y DataFrames based on the specified column.
 
-        Returns
-        -------
-        tuple of two dicts
-            The first dict contains the X dataframes and the second dict contains the y dataframes.
-            Both dicts have string keys corresponding to the names of the dataframes.
+        :param y_col: Name of the column to be used as the y values.
+        :type y_col: str
+        :returns: A tuple containing two dictionaries. The first dictionary contains the X DataFrames and the second dictionary contains the y DataFrames, both keyed by the names of the original DataFrames.
+        :rtype: Tuple[Dict[str, pd.DataFrame], Dict[str, pd.DataFrame]]
         """
         x_dfs = dict()
         y_dfs = dict()
