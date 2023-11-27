5e6f3cd2c41921374e9d161b940929bd91c1f35a
moben1 <aekmedbenabbassi@gmail.com>
2023-11-26 21:06:32 -0500

    document lasso_model.py

--- qsar/models/lasso_model.py
+++ qsar/models/lasso_model.py
@@ -11,11 +11,14 @@ class LassoModel(Model):
     def __init__(self, max_iter: int = Model.DEFAULT_MAX_ITER, random_state: int = Model.DEFAULT_RANDOM_STATE,
                  params=None):
         """
-           Initialize the Lasso model.
+        A class used to represent a LassoModel.
 
-           Parameters:
-           - max_iter: Maximum number of iterations for convergence.
-           - random_state: Seed for reproducibility.
+        :param max_iter: the maximum number of iterations for the model, defaults to Model.DEFAULT_MAX_ITER
+        :type max_iter: int, optional
+        :param random_state: the random state for the model, defaults to Model.DEFAULT_RANDOM_STATE
+        :type random_state: int, optional
+        :param params: the parameters for the model, defaults to None
+        :type params: dict, optional
         """
         super().__init__()
         self.model = Lasso(max_iter=max_iter, random_state=random_state)
@@ -23,14 +26,14 @@ class LassoModel(Model):
 
     def optimize_hyperparameters(self, trial: Trial, df: pd.DataFrame) -> float:
         """
-        Optimize the hyperparameters of the Lasso model using the given trial and data.
+        Optimizes the hyperparameters of the Lasso Regressor model.
 
-        Parameters:
-        - trial: Optuna trial for hyperparameter optimization.
-        - df: Data for cross-validation.
-
-        Returns:
-        - Cross-validation score.
+        :param trial: the trial for hyperparameter optimization
+        :type trial: Trial
+        :param df: the dataframe used for training the model
+        :type df: pd.DataFrame
+        :return: the cross validation score of the model
+        :rtype: float
         """
         self.params = {
             "alpha": trial.suggest_float("alpha", 1e-10, 1e10, log=True),
