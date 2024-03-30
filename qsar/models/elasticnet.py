45dde3ba0b40f23c36489776952b0b200929fad0
moben1 <aekmedbenabbassi@gmail.com>
2024-03-30 00:59:01 -0400

    Fix overfitting elasticnet model and update its tutorial

--- qsar/models/elasticnet.py
+++ qsar/models/elasticnet.py
@@ -44,8 +44,8 @@ class ElasticnetModel(BaselineModel):
         :rtype: float
         """
         self.params = {
-            "alpha": trial.suggest_float("alpha", 1e-10, 1e10, log=True),
-            "l1_ratio": trial.suggest_float("l1_ratio", 1e-10, 1, log=True),
+            "alpha": trial.suggest_float("alpha", 1e-5, 2, log=True),
+            "l1_ratio": trial.suggest_float("l1_ratio", 1e-2, 1, log=True),
         }
 
         self.model.set_params(**self.params)
